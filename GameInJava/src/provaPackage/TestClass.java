9032b27356307720aa1c5fb2633deb8f49b6874d
wauro <wauro@wauro-Latitude-E7250>
2021-01-24 21:47:48 +0100

    mauro branch 1

--- GameInJava/src/provaPackage/TestClass.java
+++ GameInJava/src/provaPackage/TestClass.java
@@ -4,5 +4,11 @@ public class TestClass {
 public static void main (String [] args) {
 	System.out.println("Ciao");
 	int a;
+	suca();
+}
+
+private static void suca() {
+	// TODO Auto-generated method stub
+	
 }
 }
