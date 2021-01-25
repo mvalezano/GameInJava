82e3bd6d26be81f44d03b32d78a7ada416f4a13c
andrea.miletto <andrea.miletto@hotmail.com>
2021-01-25 19:53:49 +0100

    create displayTest class

---
+++ GameInJava/src/test/codenmore/tilegame/display/DisplayTest.java
@@ -0,0 +1,25 @@
+package test.codenmore.tilegame.display;
+
+import dev.codenmore.tilegame.display.Display;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class DisplayTest {
+
+    @Test
+    public void createDisplayTest() {
+
+        Display display = new Display("Titolo", 200, 300);
+        String x = display.title;
+        assertEquals("Titolo", display.title);
+        assertEquals(200, display.width);
+        assertEquals(300, display.height);
+
+        assertEquals(300, display.frame.getSize().getHeight(), 0.0);
+        assertEquals(200, display.frame.getSize().getWidth(), 0.0);
+
+    }
+
+
+}
