221addc0502722551757024da0cc23f802f1f996
bra <bra@LAPTOP-3DJR0HTP.lan>
2021-02-02 16:04:05 +0100

    video 13
    creazione classi entity creature && player

--- GameInJava/src/dev/codenmore/tilegame/states/GameState.java
+++ GameInJava/src/dev/codenmore/tilegame/states/GameState.java
@@ -2,29 +2,33 @@ package dev.codenmore.tilegame.states;
 
 import java.awt.Graphics;
 
+import dev.codenmore.tilegame.entities.creatures.Player;
 import dev.codenmore.tilegame.gfx.Assets;
 
 public class GameState extends State
 	{
 		
+		private Player player;
+		
 		public GameState() {
-			
+			player=new Player(100,100);
 		}
 
 	@Override
 	public void tick()
 		{
-			
+			player.tick();
 			
 		}
 
 	@Override
 	public void render(Graphics g)
 		{
-			g.drawImage(Assets.tree,0,0,null);
+			player.render(g);
+			//			g.drawImage(Assets.tree,0,0,null);
 		}
 
-		
+	
 		
 		
 	}
