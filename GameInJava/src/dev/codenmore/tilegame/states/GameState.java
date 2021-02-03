c879bbfc2611b2df41fc8e386591e49becdd93fb
bra <bra@LAPTOP-3DJR0HTP.lan>
2021-02-03 11:52:15 +0100

    video 17!
    Starting worlds

--- GameInJava/src/dev/codenmore/tilegame/states/GameState.java
+++ GameInJava/src/dev/codenmore/tilegame/states/GameState.java
@@ -6,20 +6,25 @@ import dev.codenmore.tilegame.Game;
 import dev.codenmore.tilegame.entities.creatures.Player;
 import dev.codenmore.tilegame.gfx.Assets;
 import dev.codenmore.tilegame.tiles.Tile;
+import dev.codenmore.tilegame.worlds.World;
 
 public class GameState extends State
 	{
 		
 		private Player player;
+		private World world;
 		
 		public GameState(Game game) {
 			super(game);
 			player=new Player(game,100,100);
+		world= new World("");
 		}
+		
 
 	@Override
 	public void tick()
 		{
+			world.tick();
 			player.tick();
 			
 		}
@@ -27,12 +32,11 @@ public class GameState extends State
 	@Override
 	public void render(Graphics g)
 		{
+			world.render(g);
 			player.render(g);
 			//Tile.tiles[2].render(g, 0, 0);
 			//			g.drawImage(Assets.tree,0,0,null);
 		}
 
-	
-		
 		
 	}
